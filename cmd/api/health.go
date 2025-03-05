f2d551364bcb0d19ee141b1e1a979bd2aa3585f1
waditya <waditya02@gmail.com>
2025-03-05 16:22:31 -0600

    Add comments to explain the map structure for data in the health.go API

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -5,6 +5,9 @@ import (
 )
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	// Map format : map[<<data type of key>>]<<data type of value>>
+	// A map with key as string and value as Boolean will be
+	// map[string]bool
 
 	data := map[string]string{
 		"status":  "ok",
