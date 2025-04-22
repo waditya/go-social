9c4bb9a607c614ea3a168fa9ec2b8e59c432da07
waditya <waditya02@gmail.com>
2025-04-21 19:36:34 -0500

    fix: correct the typo in api posts json configuration of CreatePostPayload

--- cmd/api/posts.go
+++ cmd/api/posts.go
@@ -15,8 +15,8 @@ type postKey string
 const postCtx postKey = "post"
 
 type CreatePostPayload struct {
-	Title   string   `json:"title" validate:"required,min=5,max=100"`
-	Content string   `json:"content" validate:"required",min=5,max=1000"`
+	Title   string   `json:"title" validate:"required,max=100"`
+	Content string   `json:"content" validate:"required,max=1000"`
 	Tags    []string `json:"tags"`
 }
 
